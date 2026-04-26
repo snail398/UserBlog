9b66ddd90e834ab79a156abc4dce598ea7b43d2e
RomanShtykov <xxxromaromaxxx@gmail.com>
2026-04-26 13:19:58 +0200

    created initial project

---
+++ Common/HealthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/health")]
+public sealed class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "ok",
+            service = "UserBlog.Api"
+        });
+    }
+}
